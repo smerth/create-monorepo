d40a790112c5458787c86495dfeed5a0952397a0
Stephen Merth <stephen.merth@gmail.com>
2023-11-28 11:36:39 -0800

    Fix the path to script issue

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # glym
 
-A Gum/Shell CLI to create a Node/Lerna monorepo with two demo packages ready to publish. 
+A Gum/Shell CLI to create a Node/Lerna monorepo with two demo packages ready to publish.
+
+> To run this script you must have [gum](https://github.com/charmbracelet/gum) installed.
+
+```shell
+brew install gum
+```
--- monorepo-build-steps/create-gh-repository.zsh
+++ monorepo-build-steps/create-gh-repository.zsh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-# Introduce this step
-TEXT=("Create a repository on GitHub and clone it.")
-zsh gum-ui/banner.zsh $TEXT
+SCRIPT_DIR=$(dirname "$0")
 
+# Section Header
+TEXT=("Create a repository on GitHub and clone it.")
+zsh $SCRIPT_DIR/../gum-ui/banner.zsh $TEXT
+
+# Section Description
 gh_repo_create_description=("Lets run the 'gh repo create' command because it prompts us for all the options available to us when creating a repo on GitHub. Be sure to choose the option to clone the repo locally so we can work with it.")
-zsh gum-ui/description.zsh $gh_repo_create_description
+zsh $SCRIPT_DIR/../gum-ui/description.zsh $gh_repo_create_description
 
 # Confirm
 gum confirm "Sound good?" --affirmative="OK" --negative="Cancel" --selected.background="#0000ff" --selected.foreground="#ffffff"
